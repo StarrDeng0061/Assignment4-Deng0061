65b3b9891dd02f83d6a4b7ff5286b783d57fe11b
Starr Deng <deng0061@algonquinlive.com>
2022-12-09 23:51:55 -0500

    Inital commit

---
+++ trtest.py
@@ -0,0 +1,17 @@
+#Starr Deng
+import random
+
+n = int(input("Enter list length n:"))
+m = int(input("Enter (positive integer) m:"))
+
+cpu_nbrs = []
+for i in range(n):
+    cpu_nbrs.append(random.randint(0,m))
+
+while True:
+    inp = input("Enter guess [0 "+str(m)+"]:")
+    if int(inp) == cpu_nbrs[0]:
+        print("You win!!")
+        print("The cpu generated list:", cpu_nbrs)
+        break
+    cpu_nbrs.insert(0,cpu_nbrs.pop(n-1))
